73918a599ef2e17ac652f805313e0081d0e81b7d
wangkanai <sarin@wangkanai.com>
2025-03-09 14:34:09 +0700

    Add CoordinatePair class to represent 2D coordinate pairs

---
+++ src/Domain/CoordinatePair.cs
@@ -0,0 +1,24 @@
+namespace Wangkanai.Tiler.Domain;
+
+/// <summary>
+/// Represents a coordinate pair with X and Y values.
+/// Can be used for geographical coordinates, pixel positions, or any 2D coordinate system.
+/// </summary>
+public class CoordinatePair
+{
+    /// <summary>
+    /// X coordinate (horizontal position)
+    /// </summary>
+    public double X { get; set; }
+    
+    /// <summary>
+    /// Y coordinate (vertical position)
+    /// </summary>
+    public double Y { get; set; }
+
+
+    /// <summary>
+    /// Returns a string representation of the coordinate pair
+    /// </summary>
+    public override string ToString() => $"({X}, {Y})";
+}
