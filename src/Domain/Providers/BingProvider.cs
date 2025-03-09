878e579667fc911ec9ca25c837efb4d04a3d2855
wangkanai <sarin@wangkanai.com>
2025-03-09 17:17:04 +0700

    Add GoogleProvider and BingProvider classes
    Introduces two new provider classes, GoogleProvider and BingProvider, for handling tile URL formats. These are foundational components for integrating map services into the application.

---
+++ src/Domain/Providers/BingProvider.cs
@@ -0,0 +1,8 @@
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Tiler.Domain.Providers;
+
+public class BingProvider
+{
+	private const string UrlFormat = "http://ecn.t3.tiles.virtualearth.net/tiles/a{q}.jpeg?g=1";
+}
---
+++ src/Domain/Providers/GoogleProvider.cs
@@ -0,0 +1,8 @@
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Tiler.Domain.Providers;
+
+public class GoogleProvider
+{
+	private const string UrlFormat = "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}";
+}
