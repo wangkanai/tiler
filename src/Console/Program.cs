77fa8f864a2009e48ee3f84f6d418ef89bf4027c
wangkanai <sarin@wangkanai.com>
2025-03-31 14:58:08 +0700

    Add command-line application framework support
    Introduced a `CommandLineApplication` class to enhance command-line functionality and added UTF-8 output encoding when input is redirected. Updated project structure to include a `Commands` folder for better organization.

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -1,13 +1,16 @@
 ﻿// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
 using System.CommandLine;
+using System.Text;
 
 internal static class Program
 {
 	private static async Task<int> Main(string[] args)
 	{
-		var rootCommand = new RootCommand("Wangkanai Map Tiler CLI");
-		rootCommand.Name = "tiler";
+		if (Console.IsInputRedirected)
+			Console.OutputEncoding = Encoding.UTF8;
+
+		var rootCommand = new RootCommand("Wangkanai Map Tiler");
 
 		return await rootCommand.InvokeAsync(args);
 	}
